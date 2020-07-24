98a586d74b18326f2fbff58dcbc39f44760d3a37
Monir <zaman_uap@yahoo.com>
2020-07-23 22:00:03 -0400

    added

---
+++ ClientApp/build/precache-manifest.cfd1aaf085ac4fb403c7f40a09dcfd7c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6c02c13dbf9a80a5a3df99bd968426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9355d77387caa39e7ffa",
+    "url": "/static/css/2.54af36f7.chunk.css"
+  },
+  {
+    "revision": "8c2cf05da00c41fd7cea",
+    "url": "/static/css/main.a7a85e01.chunk.css"
+  },
+  {
+    "revision": "9355d77387caa39e7ffa",
+    "url": "/static/js/2.c0eb35f0.chunk.js"
+  },
+  {
+    "revision": "8c2cf05da00c41fd7cea",
+    "url": "/static/js/main.98851a1d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
